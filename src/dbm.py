c4434081fde7110ec073dc061aeb23dcdc638530
Francesco Pipita <francesco.pipita@gmail.com>
2011-02-20 19:26:50 +0100

    Use the `timestamp' converter to store datetime.datetime objects in the local SQLite database.

--- src/dbm.py
+++ src/dbm.py
@@ -6,59 +6,81 @@ class DBM:
     Q_CREATE_TABLE_ENTRIES = \
         """
         CREATE TABLE entry (
-           ifolder        TEXT REFERENCES ifolder (ifolder_id),
-           entry_id       TEXT,
-           mtime          DATETIME,
+           ifolder        TEXT REFERENCES ifolder (id),
+           id             TEXT,
+           mtime          timestamp,
            digest         TEXT,
-           PRIMARY KEY (ifolder, entry_id)
+           PRIMARY KEY (ifolder, id)
         )
         """
 
     Q_CREATE_TABLE_IFOLDERS = \
         """
         CREATE TABLE ifolder (
-           ifolder_id     TEXT PRIMARY KEY,
-           mtime          DATETIME
+           id             TEXT PRIMARY KEY,
+           mtime          timestamp
         )
         """
 
-    Q_ENTRY_ADD = \
+    Q_ADD_ENTRY = \
         """
         INSERT INTO entry VALUES (?, ?, ?, ?)
         """
     
-    Q_ENTRY_UPDATE = \
+    Q_UPDATE_ENTRY = \
         """
         UPDATE entry SET mtime=(?), digest=(?)
-        WHERE ifolder=(?) AND entry_id=(?)
+        WHERE ifolder=(?) AND id=(?)
+        """
+    
+    Q_GET_ENTRY = \
+        """
+        SELECT * FROM entry AS e 
+        WHERE e.ifolder=? AND e.id=?
         """
 
-    Q_ENTRY_MTIME = \
+    Q_GET_MTIME_BY_ENTRY = \
         """
         SELECT e.mtime FROM entry AS e
-        WHERE i.ifolder=? AND i.entryID=?
+        WHERE i.ifolder=? AND i.id=?
         """
 
-    Q_ENTRY_DIGEST = \
+    Q_GET_DIGEST_BY_ENTRY = \
         """
         SELECT e.digest FROM entry AS e
-        WHERE e.ifolder=? AND e.entry_id=?
+        WHERE e.ifolder=? AND e.id=?
+        """
+    Q_GET_ENTRIES_BY_IFOLDER = \
+        """
+        SELECT * FROM entry AS e WHERE e.ifolder=?
         """
 
-    Q_IFOLDER_ADD = \
+    Q_ADD_IFOLDER = \
         """
         INSERT INTO ifolder VALUES (?, ?)
         """
 
-    Q_IFOLDER_MTIME = \
+    Q_UPDATE_IFOLDER = \
+        """
+        UPDATE ifolder SET mtime=(?)
+        WHERE id=(?)
+        """
+
+    Q_GET_MTIME_BY_IFOLDER = \
         """
         SELECT i.mtime FROM ifolder AS i
-        WHERE i.ifolder_id=?
+        WHERE i.id=?
+        """
+    Q_GET_IFOLDERS = \
+        """
+        SELECT i.id, i.mtime FROM ifolder AS i
         """
 
     def __init__ (self, pathtodb):
         self.pathtodb = pathtodb
-        self.cx = sqlite3.connect (pathtodb)
+        self.cx = sqlite3.connect (pathtodb, detect_types=\
+                                       sqlite3.PARSE_DECLTYPES| \
+                                       sqlite3.PARSE_COLNAMES)
 
     def __create_tables (self):
         cu = self.cx.cursor ()
@@ -77,35 +99,51 @@ class DBM:
         finally:
             self.cx.commit ()
 
-    def ifolder_add (self, ifolder):
+    def add_ifolder (self, ifolder):
         cu = self.cx.cursor ()
-        cu.execute (DBM.Q_IFOLDER_ADD, (ifolder.ID, ifolder.LastModified))
+        cu.execute (DBM.Q_ADD_IFOLDER, (ifolder.ID, ifolder.LastModified))
         self.cx.commit ()
 
-    def entry_add (self, ifolder, change, digest):
+    def get_ifolders (self):
         cu = self.cx.cursor ()
-        cu.execute (DBM.Q_ENTRY_ADD, (ifolder.ID, change.ID, change.Time, digest))
+        cu.execute (DBM.Q_GET_IFOLDERS)
+        return cu.fetchall ()
+
+    def update_ifolder (self, ifolder_id, mtime):
+        cu = self.cx.cursor ()
+        cu.execute (DBM.Q_UPDATE_IFOLDER, (mtime, ifolder_id))
         self.cx.commit ()
 
-    def entry_update (self, ifolder, change, digest):
+    def add_entry (self, ifolder_id, change, digest):
         cu = self.cx.cursor ()
-        cu.execute (DBM.Q_ENTRY_UPDATE, \
-                        (change.Time, digest, ifolder.ID, change.ID))
+        cu.execute (DBM.Q_ADD_ENTRY, (ifolder_id, change.ID, change.Time, digest))
+        self.cx.commit ()
+
+    def update_entry (self, ifolder_id, change, digest):
+        cu = self.cx.cursor ()
+        cu.execute (DBM.Q_UPDATE_ENTRY, \
+                        (change.Time, digest, ifolder_id, change.ID))
         self.cx.commit ()
     
-    def get_entry_mtime (self, ifolder, change):
+    def get_mtime_by_entry (self, ifolder, change):
         cu = self.cx.cursor ()
-        cu.execute (DBM.Q_ENTRY_MTIME, (ifolder.ID, change.ID))
-        row = cu.fetchone ()
-        mtime = datetime.strptime (row[0], '%Y-%m-%d %H:%M:%S.%f')
-        return mtime
+        cu.execute (DBM.Q_GET_ENTRY_MTIME, (ifolder.ID, change.ID))
+        return cu.fetchone ()
 
-    def get_entry_digest (self, ifolder, change):
+    def get_digest_by_entry (self, ifolder, change):
         cu = self.cx.cursor ()
-        cu.execute (DBM.Q_ENTRY_DIGEST, (ifolder.ID, change.ID))
-        row = cu.fetchone ()
-        digest = row[0]
-        return digest
+        cu.execute (DBM.Q_GET_ENTRY_DIGEST, (ifolder.ID, change.ID))
+        return cu.fetchone ()
+
+    def get_entry (self, ifolder_id, entry_id):
+        cu = self.cx.cursor ()
+        cu.execute (DBM.Q_GET_ENTRY, (ifolder_id, entry_id))
+        return cu.fetchone ()
+
+    def get_entries_by_ifolder (self, ifolder_id):
+        cu = self.cx.cursor ()
+        cu.execute (DBM.Q_GET_ENTRIES_BY_IFOLDER, (ifolder_id,))
+        return cu.fetchall ()
 
     def __del__ (self):
         self.cx.commit ()
