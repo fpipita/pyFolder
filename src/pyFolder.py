53115bcafc3dd2d2155d4951e5c20325b9a83c81
Francesco Pipita <francesco.pipita@gmail.com>
2011-02-21 13:26:18 +0100

    Added the `file_has_local_changes' method and added the `debug' method to the public interface.

--- src/pyFolder.py
+++ src/pyFolder.py
@@ -46,7 +46,7 @@ class pyFolder:
         return self.client.service.GetiFolder (ifolder_id)
 
     def fetch (self, ifolder_id, entry_id, path):
-        self.__debug ('Fetching file \'{0}\' ...'.format (path), False)
+        self.debug ('Fetching file \'{0}\' ...'.format (path), False)
         handle = self.client.service.OpenFileRead (ifolder_id, entry_id)
         with open (path, 'wb') as f:
             while True:
@@ -56,26 +56,45 @@ class pyFolder:
                     break
                 f.write (base64.b64decode (b64data))
             self.client.service.CloseFile (handle)
-        self.__debug ('done')
+        self.debug ('done')
 
     def delete (self, path):
         if os.path.isfile (path):
-            self.__debug ('Deleting file \'{0}\' ...'.format (path), False)
+            self.debug ('Deleting file \'{0}\' ...'.format (path), False)
             os.remove (path)
-            self.__debug ('done')
+            self.debug ('done')
 
     def rmdir (self, path):
         if os.path.isdir (path):
-            self.__debug ('Removing directory \'{0}\' ...'.format (path), False)
+            self.debug ('Removing directory \'{0}\' ...'.format (path), False)
             shutil.rmtree (path)
-            self.__debug ('done')
+            self.debug ('done')
 
     def mkdir (self, path):
         if not os.path.isdir (path):
-            self.__debug ('Adding directory \'{0}\' ...'.format (path), False)
+            self.debug ('Adding directory \'{0}\' ...'.format (path), False)
             os.makedirs (path)
-            self.__debug ('done')
+            self.debug ('done')
 
+    def file_has_local_changes (self, ifolder_id, entry_id, name):
+        row = self.dbm.get_digest_by_entry (ifolder_id, entry_id)
+        if not os.path.exists (name):
+            if row is None:
+                return False
+            else:
+                return True
+        else:
+            if row is None:
+                return True
+            else:
+                old_digest = row[0]
+                new_digest = self.__md5_hash (name)
+                if old_digest != new_digest:
+                    return True
+                else:
+                    return False
+        return True
+        
     def __add_ifolder (self, ifolder_id, mtime, name):
         update_dbm = self.conflicts_handler.add_directory (None, None, name)
         if update_dbm:
@@ -110,7 +129,7 @@ class pyFolder:
                     (ifolder_id, change.ID, change.Name)
             if update_dbm:
                 self.dbm.add_entry \
-                    (ifolder_id, change, self.__md5_hash (change))
+                    (ifolder_id, change, self.__md5_hash (change.Name))
         return update_dbm
         
     def checkout (self):
@@ -152,7 +171,7 @@ class pyFolder:
         update_dbm = False
         for entry_t in entries_t:
             if self.__entry_has_changes (entry_t):
-                self.__debug ('Entry {0} has remote changes'.format (entry_t[1]))
+                self.debug ('Entry {0} has remote changes'.format (entry_t[1]))
                 iet = self.client.factory.create ('iFolderEntryType')
                 cea = self.client.factory.create ('ChangeEntryAction')
                 latest_change = self.__get_latest_change \
@@ -191,7 +210,7 @@ class pyFolder:
                                 change.Action == cea.Modify:
                             self.dbm.update_mtime_and_digest_by_entry (\
                                 ifolder_t[0], change, \
-                                    self.__md5_hash (change))
+                                    self.__md5_hash (change.Name))
                         elif change.Action == cea.Delete:
                             self.dbm.delete_entry (ifolder_t[0], change.ID)
         return update_dbm
@@ -258,7 +277,7 @@ class pyFolder:
         known_ifolders_t = self.dbm.get_ifolders ()
         for ifolder_t in known_ifolders_t:
             if self.__ifolder_has_changes (ifolder_t):
-                self.__debug ('iFolder {0} has remote changes'.format (ifolder_t[0]))
+                self.debug ('iFolder {0} has remote changes'.format (ifolder_t[0]))
                 update_dbm = update_dbm or self.__update_ifolder (ifolder_t)
                 update_dbm = update_dbm or self.__add_new_entries (ifolder_t)
                 if update_dbm:
@@ -269,22 +288,22 @@ class pyFolder:
             self.__check_for_deleted_membership (ifolder_t)
         self.__add_new_ifolders ()
 
-    def __md5_hash (self, change):
+    def __md5_hash (self, name):
         md5_hash = 'DIRECTORY'
-        if os.path.isfile (change.Name):
-            self.__debug ('MD5SUM (\'{0}\') ->'.format (change.Name), False)
+        if os.path.isfile (name):
+            self.debug ('MD5SUM (\'{0}\') ->'.format (name), False)
             m = hashlib.md5 ()
-            with open (change.Name, 'rb') as f:
+            with open (name, 'rb') as f:
                 while True:
                     data = f.read ()
                     m.update (data)
                     if len (data) == 0:
                         break
                 md5_hash = m.hexdigest ()
-                self.__debug ('{0}'.format (md5_hash, True))
+                self.debug ('{0}'.format (md5_hash, True))
         return md5_hash
 
-    def __debug (self, message, newline=True):
+    def debug (self, message, newline=True):
         if self.cm.get_verbose ():
             if newline:
                 print >> sys.stderr, message
