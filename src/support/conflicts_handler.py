63955d165a0e7a25d730fd0ac706f98fa41b9112
Francesco Pipita <francesco.pipita@gmail.com>
2011-02-24 11:31:36 +0100

    Added a `DEFAULT' Policy.

--- src/support/conflicts_handler.py
+++ src/support/conflicts_handler.py
@@ -35,110 +35,60 @@ class Policy:
     def delete_remote_file (self, ifolder_id, entry_id, path):
         raise NotImplementedError
 
-class AlwaysAcceptRemoteChanges (Policy):
+class DEFAULT (Policy):
+    """
+    The DEFAULT Policy has the following features:
+
+    [ UPDATE behavior ]
+    - If an entry has any kind of remote change, the changes are also applied
+      locally.
+    - If a new entry is added remotely, it is also added to the local
+      repository.
+
+    [ COMMIT behavior ]
+    - If an entry has local changes (modify, deletion), changes are committed.
+    - New locally added entries are committed. If, at the time of the commit,
+      a remote entry having the same path and name of the one that is being
+      committed has been added, then the entries are renamed, adding a suffix
+      with the `OwnerUserName' and both the copies are saved on the server, so
+      that they will be available for all the users at the next update.
+    """
     def add_directory (self, ifolder_id, entry_id, path):
-        self.pyFolder.mkdir (path)
-        return True
+        return self.pyFolder.mkdir (path)
 
     def add_file (self, ifolder_id, entry_id, path):
-        self.pyFolder.fetch (ifolder_id, entry_id, path)
-        return True
+        return self.pyFolder.fetch (ifolder_id, entry_id, path)
 
     def modify_directory (self, ifolder_id, entry_id, path):
         return True
 
     def modify_file (self, ifolder_id, entry_id, path):
-        self.pyFolder.fetch (ifolder_id, entry_id, path)
-        return True
+        return self.pyFolder.fetch (ifolder_id, entry_id, path)
     
     def delete_directory (self, ifolder_id, entry_id, path):
-        self.pyFolder.rmdir (path)
-        return True
+        return self.pyFolder.rmdir (path)
 
     def delete_file (self, ifolder_id, entry_id, path):
-        self.pyFolder.delete (path)
-        return True
+        return self.pyFolder.delete (path)
 
     def modify_remote_directory (self, ifolder_id, entry_id, path):
         return False
     
     def modify_remote_file (self, ifolder_id, entry_id, path):
-        return False
+        return self.pyFolder.remote_file_write (ifolder_id, entry_id, path)
     
     def delete_remote_directory (self, ifolder_id, entry_id, path):
-        return False
+        return self.pyFolder.remote_rmdir (ifolder_id, entry_id, path)
 
     def delete_remote_file (self, ifolder_id, entry_id, path):
-        return False
-
-class AlwaysKeepLocalChanges (Policy):
-    def add_directory (self, ifolder_id, entry_id, path):
-        if self.pyFolder.directory_has_local_changes (ifolder_id, entry_id, path):
-            return False
-        else:
-            self.pyFolder.mkdir (path)
-            return True
-
-    def add_file (self, ifolder_id, entry_id, path):
-        if self.pyFolder.file_has_local_changes (ifolder_id, entry_id, path):
-            return False
-        else:
-            self.pyFolder.fetch (ifolder_id, entry_id, path)
-            return True
-
-    def modify_directory (self, ifolder_id, entry_id, path):
-        if self.pyFolder.directory_has_local_changes (ifolder_id, entry_id, path):
-            return False
-        else:
-            return True
-
-    def modify_file (self, ifolder_id, entry_id, path):
-        if self.pyFolder.file_has_local_changes (ifolder_id, entry_id, path):
-            return False
-        else:
-            self.pyFolder.fetch (ifolder_id, entry_id, path)
-            return True
-    
-    def delete_directory (self, ifolder_id, entry_id, path):
-        if self.pyFolder.directory_has_local_changes (ifolder_id, entry_id, path):
-            return False
-        else:
-            self.pyFolder.rmdir (path)
-            return True
-
-    def delete_file (self, ifolder_id, entry_id, path):
-        if self.pyFolder.file_has_local_changes (ifolder_id, entry_id, path):
-            return False
-        else:
-            self.pyFolder.delete (path)
-            return True
-
-    def modify_remote_directory (self, ifolder_id, entry_id, path):
-        return False
-    
-    def modify_remote_file (self, ifolder_id, entry_id, path):
-        self.pyFolder.remote_file_write (ifolder_id, entry_id, path)
-        return True
-    
-    def delete_remote_directory (self, ifolder_id, entry_id, path):
-        self.pyFolder.remote_rmdir (ifolder_id, entry_id, path)
-        return True
-
-    def delete_remote_file (self, ifolder_id, entry_id, path):
-        self.pyFolder.remote_delete (ifolder_id, entry_id, path)
-        return True
+        return self.pyFolder.remote_delete (ifolder_id, entry_id, path)
 
 class ConflictsHandlerFactory:
     @staticmethod
     def create (policy, pyFolder):
-        if policy == 'AlwaysAcceptRemoteChanges':
-            return AlwaysAcceptRemoteChanges (pyFolder)
-        elif policy == 'AlwaysKeepLocalChanges':
-            return AlwaysKeepLocalChanges (pyFolder)
+        if policy == 'DEFAULT':
+            return DEFAULT (pyFolder)
     
     @staticmethod
     def get_factories ():
-        return [\
-            'AlwaysAcceptRemoteChanges', \
-                'AlwaysKeepLocalChanges' \
-                ]
+        return ['DEFAULT', ]
