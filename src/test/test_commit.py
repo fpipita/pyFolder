9163f1481c4003fcfec7e91da0fb1c4b84935c6f
Francesco Pipita <francesco.pipita@gmail.com>
2011-03-01 11:08:30 +0100

    Added one more test.

--- src/test/test_commit.py
+++ src/test/test_commit.py
@@ -24,10 +24,8 @@ WAIT_FOR_SIMIAS_TO_UPDATE = 5
 class TestCommit (unittest.TestCase):
 
     def setUp (self):
-        self.cm = CfgManager (\
-            pathtodb=':memory:', \
-            soapbuflen=DEFAULT_SOAP_BUFLEN, \
-                runfromtest=True)
+        self.cm = CfgManager (soapbuflen=DEFAULT_SOAP_BUFLEN, \
+                                  pathtodb=':memory:', runfromtest=True)
         self.cm.options.username = USERNAME
         self.cm.options.password = PASSWORD
         self.cm.options.ifolderws = IFOLDERWS
@@ -60,6 +58,58 @@ class TestCommit (unittest.TestCase):
         
         self.assertFalse (self.pyFolder.is_new_local_directory (\
                 iFolderEntry.iFolderID, os.path.normpath (iFolderEntry.Path)))
+        
+    def testAddEntryWhosePathHasAlreadyBeenAdded (self):
+        Name = 'foo'
+        Data = 'bar'
+
+        _USERNAME = 'mario'
+        _PASSWORD = 'foo'
+        _PATHTODB = os.path.join (PREFIX, 'bar')
+
+        Rights = self.pyFolder.ifolderws.get_rights ()
+        SearchOperation = self.pyFolder.ifolderws.get_search_operation ()
+        SearchProperty = self.pyFolder.ifolderws.get_search_property ()
+
+        ArrayOfiFolderUser = self.pyFolder.ifolderws.get_users_by_search (\
+            SearchProperty.UserName, SearchOperation.Contains, _USERNAME, \
+                0, 1)
+
+        if ArrayOfiFolderUser is not None:
+            for iFolderUser in ArrayOfiFolderUser:
+                self.pyFolder.ifolderws.add_member (\
+                    self.iFolder.ID, iFolderUser.ID, Rights.ReadWrite)
+        else:
+            self.fail ()
+            return
+
+        _cm = CfgManager (soapbuflen=DEFAULT_SOAP_BUFLEN, runfromtest=True)
+        _cm.options.username = _USERNAME
+        _cm.options.password = _PASSWORD
+        _cm.options.ifolderws = IFOLDERWS
+        _ifolderws = iFolderWS (_cm)
+
+        logging.getLogger ('suds.client').addHandler (NullHandler ())
+
+        iFolderEntry = _ifolderws.create_entry (\
+            self.iFolder.ID, self.iFolderEntry.ID, Name, \
+                self.iFolderEntryType.File)
+        
+        time.sleep (WAIT_FOR_SIMIAS_TO_UPDATE)
+
+        LocalPath = os.path.join (PREFIX, iFolderEntry.Path)
+
+        with open (LocalPath, 'wb') as File:
+            File.write (Data)
+            
+        self.pyFolder.commit ()
+        
+        time.sleep (WAIT_FOR_SIMIAS_TO_UPDATE)
+
+        self.pyFolder.update ()
+
+        self.assertTrue (os.path.exists ('{0}-{1}'.format (LocalPath, USERNAME)))
+        self.assertTrue (os.path.exists (LocalPath))
         
 if __name__ == '__main__':
     unittest.main ()
